1013db3fc9f532983bce1e6ff105119f3810a8a9
AracelySC <arasaenz15@gmail.com>
2024-01-29 22:54:14 -0600

    Navbar & information
    Navbar, effect mouse and information

--- personal-portfolio/src/assets/lenguage/information.js
+++ personal-portfolio/src/assets/lenguage/information.js
@@ -1,4 +1,42 @@
-const information ={
-    es:{},
-    en:{}
-}
+const information = {
+    ES: {
+        nav: {
+            home: 'Inicio',
+            about: 'Sobre mi',
+            resume: 'CV',
+            skills: 'Habilidades',
+            projects: 'Proyectos',
+            contact: 'Contacto',
+        },
+        home: {
+            title: 'Hola, mi nombre es Aracely Sáenz Contreras',
+            subtitle: 'Soy Ingeniero de Software y Desarrollador Fullstack',
+            welcome:'Bienvenido a mi portafolio',
+         },
+         about:{
+            description: 'Bachiller en Informática en la Universidad de Costa Rica con una sólida formación académica en programación, bases de datos y gestión de proyectos. Me considero una persona proactiva, autodidacta y apasionada por lo que hago.',
+
+         },
+    },
+    EN: {
+        nav: {
+            home: 'Home',
+            about: 'About Me',
+            resume: 'Resume',
+            skills: 'Skills',
+            projects: 'Projects',
+            contact: 'Contact',
+        },
+        home:{
+            title:'Hello, my name is Aracely Sáenz Contreras',
+            subtitle:'I am Software Engineer & Fullstack Developer',
+            welcome:'Welcome to my portfolio',
+        },
+        about:{
+            description:'Bachelor in Computer Science at the University of Costa Rica with a solid academic background in programming, databases and project management. I consider myself a proactive person, self-taught and passionate about what I do.',
+
+        },
+    },
+};
+
+export default information;
