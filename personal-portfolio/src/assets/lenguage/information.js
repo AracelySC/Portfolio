285634c78d8647b490700ed4bafe607aeeb1b224
AracelySC <arasaenz15@gmail.com>
2024-03-18 13:06:07 -0600

    Update information.js

--- personal-portfolio/src/assets/lenguage/information.js
+++ personal-portfolio/src/assets/lenguage/information.js
@@ -15,8 +15,43 @@ const information = {
          },
          about:{
             description: 'Bachiller en Informática en la Universidad de Costa Rica con una sólida formación académica en programación, bases de datos y gestión de proyectos. Me considero una persona proactiva, autodidacta y apasionada por lo que hago.',
-
-         },
+        },
+        resume:{
+            title:'Mi CV',
+            education:[{
+                university:'Universidad de Costa Rica',
+                degree:'Bachillerato en Informática Empresarial',
+                date:'2018 - 2023',
+            },
+            {   university:'Colegio Técnico Profesional de Oreamuno',
+                degree:'Técnico Medio en Informática de Desarrollo de Software',
+                date:'2015 - 2017',
+            }],
+            experience:{
+                company:'Universidad de Costa Rica - Proyecto de Investigación FUNREAD',
+                position:'Líder de equipo y Desarrollador web',
+                describe:"Como pasante trabajé con tecnologías como Jira, React, Bootstrap, JavaScript, Python y aplicativos inteligentes, utilizando la metodología Scrumban.",
+                date:'01/2023 - 12/2023',
+            },
+            skills:{ 
+                languages:['Español (Nativo)', 'Inglés (B1)'],   
+                programmingLanguages:['Java', 'Python', 'JavaScript', 'C#', 'HTML', 'CSS', 'SASS'],
+                databases:['SQL Server', 'MySQL', 'PostgreSQL'],
+                tools:['Git','Github', 'Jira', 'Postman', 'Visual Studio', 'Visual Studio Code'],
+                framework:['.NET','React', 'Angular', 'Bootstrap', 'Node.js', 'Express']
+                
+            },
+        },
+        projects:[{
+            title:'Proyectos',
+            description:'Estos son algunos de los proyectos en los que he trabajado. Si quieres saber más, no dudes en contactarme.',
+        },{}],
+        contact:{
+            title:'Contactame',  
+            email:'arasaenz15@gmail.com',
+            phoneNumber:'(+506) 8637 9015',
+            address:'Cartago, Costa Rica',     
+        },
     },
     EN: {
         nav: {
@@ -34,8 +69,43 @@ const information = {
         },
         about:{
             description:'Bachelor in Computer Science at the University of Costa Rica with a solid academic background in programming, databases and project management. I consider myself a proactive person, self-taught and passionate about what I do.',
-
         },
+        resume:{
+            title:'My Resume',
+            education:[{
+                university:'University of Costa Rica',
+                degree:'Bachelor in Business Informatics',
+                date:'2018 - 2023',
+            },
+            {   university:'Technical Professional College of Oreamuno',
+                degree:'Middle Technician in Software Development Informatics',
+                date:'2015 - 2017',
+            }],
+            experience:{
+                company:'University of Costa Rica - FUNREAD Research Project',
+                position:'Team Leader and Web Developer',
+                describe:"As an intern, I worked with technologies such as Jira, React, Bootstrap, JavaScript, Python and intelligent applications, using the Scrumban methodology.",
+                date:'01/2023 - 12/2023',
+            },
+            skills:{ 
+                languages:['Spanish (Native)', 'English (B1)'],   
+                programmingLanguages:['Java', 'Python', 'JavaScript', 'C#', 'HTML', 'CSS', 'SASS'],
+                databases:['SQL Server', 'MySQL', 'PostgreSQL'],
+                tools:['Git','Github', 'Jira', 'Postman', 'Visual Studio', 'Visual Studio Code'],
+                framework:['.NET','React', 'Angular', 'Bootstrap', 'Node.js', 'Express']
+            },
+        },  
+        projects:[{
+            title:'Projects',
+            description:'These are some of the projects I have worked on. If you want to know more, do not hesitate to contact me.',
+        },{}],
+        contact:{
+            title:'Contact Me',  
+            email:'aracelysaenzc@gmail.com',
+            phoneNumber:'(+506) 8637 9015',
+            address:'Cartago, Costa Rica',
+        } 
+ 
     },
 };
 
